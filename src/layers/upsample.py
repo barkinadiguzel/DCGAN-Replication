6a45362371747de6937b5e4d17815213b831209b
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2026-04-01 11:02:58 +0300

    Delete src/layers/upsample.py

--- src/layers/upsample.py
+++
@@ -1,5 +0,0 @@
-import torch.nn as nn
-
-def upsample_block(in_channels, out_channels, kernel_size=4, stride=2, padding=1, final_layer=False):
-    from src.backbone.blocks import GBlock
-    return GBlock(in_channels, out_channels, kernel_size, stride, padding, final_layer)
