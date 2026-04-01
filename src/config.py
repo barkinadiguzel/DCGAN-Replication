81a68ad41660b22a9b855adc6b77b04486c1076c
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2026-04-01 10:52:19 +0300

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,8 @@
+latent_dim = 100       
+image_size = 64       
+channels = 3          
+lr = 0.0002           
+beta1 = 0.5            
+batch_size = 128
+num_epochs = 25
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
